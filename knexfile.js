769c0d5cc9cfc01c37bffcdf207e3deb5ce319e6
Germaine Pannell <me@germainep.com>
2020-05-20 21:40:56 -0400

    continued setting up knex for project
    Preparing to install express and start on the basic auth end points

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,38 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/devdesk.db3',
+    },
+    migrations: {
+      directory: './data/migrations/',
+    },
+    seeds: {
+      directory: './data/seeds/',
+    },
   },
 
   staging: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
+      host: '127.0.0.1',
+      database: 'devdesk',
+      user: 'postgres',
+      password: process.env.PG_PASSWORD,
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './data/migrations/',
+    },
+    seeds: {
+      directory: './data/seeds/',
+    },
   },
 
   production: {
@@ -30,15 +40,14 @@ module.exports = {
     connection: {
       database: 'my_db',
       user: 'username',
-      password: 'password'
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: 'knex_migrations',
+    },
+  },
 }
