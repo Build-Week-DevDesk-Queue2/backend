45def12f034a1bcdfa5ff4565cf8fbd80875eb7a
Germaine Pannell <me@germainep.com>
2020-05-21 20:47:01 -0400

    created database schema

--- data/migrations/20200520213443_initial.js
+++ data/migrations/20200520213443_initial.js
@@ -1,3 +1,25 @@
-exports.up = function ( knex ) {}
+exports.up = function ( knex ) {
+  return knex.schema
+             .createTable('users', ( tbl ) => {
+               tbl.increments()
+               tbl.string('username', 16).unique().notNullable()
+               tbl.string('password', 16).notNullable()
+               tbl.string('role').notNullable()
+             })
+             .createTable('queue', ( tbl ) => {
+               tbl.increments()
+               tbl.string('title').notNullable()
+               tbl.string('issue_type').notNullable()
+               tbl.text('have_tried')
+               tbl.text('info').notNullable()
+               tbl.integer('created_by').unsigned().references('id').inTable('users')
+               tbl.date('created_on').defaultTo(Date.now())
+               tbl.boolean('assigned').defaultTo(false)
+               tbl.integer('assigned_to').unsigned().references('id').inTable('users')
+               tbl.boolean('completed').defaultTo(false)
+             })
+}
 
-exports.down = function ( knex ) {}
+exports.down = function ( knex ) {
+  return knex.schema.dropTableIfExists('queue').dropTableIfExists('users')
+}
